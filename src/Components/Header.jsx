bd93cdd2af239d55997f5115c63e793702142d26
Priyanshu Kumar <priyanshusinghania2@gmail.com>
2025-11-14 01:07:55 +0530

    hedder

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,9 +8,9 @@ const Header = ({ cartCount, setIsCartOpen }) => {
       <header className="bg-white px-12 py-3 flex justify-between items-center shadow-md fixed w-full top-0 z-50">
       <div className="text-5xl font-bold text-red-600 italic">KFC</div>
       <nav className="flex gap-8 items-center">
-        <a href="#about" className="text-red-600 font-bold text-sm hidden lg:flex hover:opacity-80 transition">ABOUT US</a>
-        <a href="#find" className="text-red-600 font-bold text-sm hidden lg:flex hover:opacity-80 transition">FIND US</a>
-        <a href="#campaigns" className="text-red-600 font-bold text-sm hidden lg:flex hover:opacity-80 transition">ALL CAMPAIGNS</a>
+        <a href="#about" className="text-red-600 font-bold text-sm hidden md:flex hover:opacity-80 transition">ABOUT US</a>
+        <a href="#find" className="text-red-600 font-bold text-sm hidden md:flex hover:opacity-80 transition">FIND US</a>
+        <a href="#campaigns" className="text-red-600 font-bold text-sm hidden md:flex hover:opacity-80 transition">ALL CAMPAIGNS</a>
         <div className="flex gap-4">
           <div className="bg-white rounded-full w-10 h-10 flex items-center justify-center cursor-pointer shadow">
         <i className="ri-user-2-fill text-2xl lg:text-[18px]  text-red-600"></i>
