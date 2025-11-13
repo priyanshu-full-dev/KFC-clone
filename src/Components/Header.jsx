629724d453ca89b90af322694e1ae095c82dcb89
Priyanshu Kumar <priyanshusinghania2@gmail.com>
2025-11-14 00:25:12 +0530

    Update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,18 +8,19 @@ const Header = ({ cartCount, setIsCartOpen }) => {
       <header className="bg-white px-12 py-3 flex justify-between items-center shadow-md fixed w-full top-0 z-50">
       <div className="text-5xl font-bold text-red-600 italic">KFC</div>
       <nav className="flex gap-8 items-center">
-        <a href="#about" className="text-red-600 font-bold text-sm hover:opacity-80 transition">ABOUT US</a>
-        <a href="#find" className="text-red-600 font-bold text-sm hover:opacity-80 transition">FIND US</a>
-        <a href="#campaigns" className="text-red-600 font-bold text-sm hover:opacity-80 transition">ALL CAMPAIGNS</a>
+        <a href="#about" className="text-red-600 font-bold text-sm hidden lg:flex hover:opacity-80 transition">ABOUT US</a>
+        <a href="#find" className="text-red-600 font-bold text-sm hidden lg:flex hover:opacity-80 transition">FIND US</a>
+        <a href="#campaigns" className="text-red-600 font-bold text-sm hidden lg:flex hover:opacity-80 transition">ALL CAMPAIGNS</a>
         <div className="flex gap-4">
           <div className="bg-white rounded-full w-10 h-10 flex items-center justify-center cursor-pointer shadow">
-        <i className="ri-user-2-fill  text-red-600"></i>
+        <i className="ri-user-2-fill text-2xl lg:text-[18px]  text-red-600"></i>
           </div>
           <div 
             onClick={() => setIsCartOpen(true)}
             className="bg-white rounded-full w-10 h-10 flex items-center justify-center cursor-pointer shadow relative hover:bg-gray-50  transition"
           >
-            <i className="ri-shopping-cart-2-fill text-red-600 "></i>
+        
+            <i className="ri-shopping-cart-2-fill text-2xl lg:text-[18px]  text-red-600 "></i>
             
             {cartCount > 0 && (
               <span className="absolute right-0 top-0 text-center font-bold  text-red-600 rounded-full w-5 h-5 text-xs flex items-center justify-center">
